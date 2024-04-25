ac7fb1167e498d415c845f5dd44fe4fcd6bceadb
fatekkl <mathtml.1105@gmail.com>
2024-04-25 09:14:10 -0300

    i commented to dont show errors in my IDE cause i dont declare functions in code, leetcode provider

--- Fundamental/BinarySearch/BinarySearch.kt
+++ Fundamental/BinarySearch/BinarySearch.kt
@@ -1,21 +1,21 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      fun isBadVersion(version: Int) : Boolean {} */
+// /* The isBadVersion API is defined in the parent class VersionControl.
+//       fun isBadVersion(version: Int) : Boolean {} */
 
-class Solution: VersionControl() {
-    override fun firstBadVersion(n: Int) : Int {
-        var l = 1
-        var r = n
+// class Solution: VersionControl() {
+//     override fun firstBadVersion(n: Int) : Int {
+//         var l = 1
+//         var r = n
 
-        while (l <= r){
-            val mid = l + (r - l).shr(1)
+//         while (l <= r){
+//             val mid = l + (r - l).shr(1)
 
-            if (isBadVersion(mid)){
-                r = mid - 1
-            } else {
-                l = mid + 1
-            }
-        }
+//             if (isBadVersion(mid)){
+//                 r = mid - 1
+//             } else {
+//                 l = mid + 1
+//             }
+//         }
 
-        return l
-	}
-}
+//         return l
+// 	}
+// }

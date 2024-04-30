97e37a14faa56170d4e5116223617600bec3291f
fatekkl <mathtml.1105@gmail.com>
2024-04-30 09:57:56 -0300

    refactor to optimize runtime to O(n)

--- Fundamental/ArrayExercices/MissingNumber/MissingNumber.js
+++ Fundamental/ArrayExercices/MissingNumber/MissingNumber.js
@@ -4,19 +4,14 @@
  */
 var missingNumber = function (nums) {
     const n = nums.length
-    const arr = []
 
-    // cria arr com numeros de 1 a n
     for (let i = 0; i <= n; i++) {
-        arr.push(i)
-    }
-
-    for (let i = 0; i <= arr.length - 1; i++) {
-        if (!nums.includes(arr[i])) {
-            return arr[i]
+        if (!nums.includes(i)){
+            return i
         }
+
     }
 
-
+// REFACTOR
 
 };

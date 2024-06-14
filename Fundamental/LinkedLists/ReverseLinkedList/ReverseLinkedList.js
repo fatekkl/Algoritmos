499d02607b36a5db0e4841596c5c948e6e2a8a65
fatekkl <mathtml.1105@gmail.com>
2024-06-14 08:31:33 -0300

    add reverseList

---
+++ Fundamental/LinkedLists/ReverseLinkedList/ReverseLinkedList.js
@@ -0,0 +1,7 @@
+var reverseList = function(head) {
+    let [prev, current] = [null, head]
+    while(current) {
+        [current.next, prev, current] = [prev, current, current.next]
+    }
+    return prev
+}

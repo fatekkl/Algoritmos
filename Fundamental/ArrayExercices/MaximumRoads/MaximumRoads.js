bba187104c32cf82b465332cff30e95196cbb6e5
fatekkl <mathtml.1105@gmail.com>
2024-06-28 10:47:24 -0300

    add MaximumRoads.js

---
+++ Fundamental/ArrayExercices/MaximumRoads/MaximumRoads.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var maximumImportance = function(n, roads) {
+ 
+    const cities = new Array(n).fill(0)
+    for(let i=0;i<roads.length;i++){
+        const a=roads[i][0], b=roads[i][1]
+        cities[a]++
+        cities[b]++
+       
+    }
+   
+     return cities.sort((a,b)=>a-b).reduce((a,c,i)=>a+((i+1)*c),0)
+};

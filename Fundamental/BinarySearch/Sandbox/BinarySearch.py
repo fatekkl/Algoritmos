dbede399d7b1dec145215c851c047f776e3c61d6
fatekkl <mathtml.1105@gmail.com>
2025-03-07 11:46:07 -0300

    resolve new exercices + sandbox

--- Fundamental/BinarySearch/Sandbox/BinarySearch.py
+++ Fundamental/BinarySearch/Sandbox/BinarySearch.py
@@ -1,6 +1,4 @@
-def binary_search(nums, n):
-    l = 0
-    h = len(nums)
+def binary_search(nums, n, l, h):
     steps = 0
 
     while l < h:
@@ -23,6 +21,21 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
-result = binary_search(b, 3)
+def exponential_search(arr, target):
+    if arr[0] == target:
+        return target
+    n = len(arr)
+    i = 1
+
+    while i < n and arr[i] < target:
+        i *= 2
+
+    if arr[i] == target:
+        return i
+
+    return binary_search(arr, target, i // 2, min(i, n - 1))
+
+
+result = exponential_search(c, 11)
 
 print(result)

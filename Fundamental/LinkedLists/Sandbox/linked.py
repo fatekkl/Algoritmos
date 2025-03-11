c3fa82da9c92c9a626b842b28b76fdacc5c3d154
fatekkl <mathtml.1105@gmail.com>
2025-03-11 13:47:24 -0300

    implement linked List in py

---
+++ Fundamental/LinkedLists/Sandbox/linked.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_front(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        if self.head:
+            self.head.prev = new_node
+        else:
+            self.tail = new_node
+        self.head = new_node
+        pass
+
+    def add_to_end(self, value):
+        new_node = Node(value)
+        new_node.prev = self.tail
+        if self.tail:
+            self.tail.next = new_node
+        else:
+            self.head = new_node
+        self.tail = new_node
+        pass
+
+    def remove_from_front(self):
+        if not self.head:
+            return None
+        removed = self.head.value
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+
+        return removed
